67b3dae9602a215c43ceaec91845ad81184c5438
clampz <david@weinman.com>
2017-11-08 14:20:59 -0800

    fixed vagrant_setup

--- vagrant_setup.sh
+++ vagrant_setup.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
 # fix
-echo "set grub-pc/install_devices /dev/sda" | sudo debconf-communicate
+#echo "set grub-pc/install_devices /dev/sda" | sudo debconf-communicate
 
 # Updates
 sudo apt-get -y update
 sudo apt-get -y upgrade
 
 sudo apt-get -y install python3-pip
-sudo apt-get -y install screen
 sudo apt-get -y install gdb gdb-multiarch
 sudo apt-get -y install unzip
 sudo apt-get -y install build-essential
 sudo apt-get -y install unrar
-sudo apt-get -y install foremost
 sudo apt-get -y install htop
 
 # QEMU with MIPS/ARM - http://reverseengineering.stackexchange.com/questions/8829/cross-debugging-for-mips-elf-with-qemu-toolchain
@@ -32,7 +30,6 @@ sudo apt-get -y install libc6-arm-cross
 mkdir /etc/qemu-binfmt
 ln -s /usr/mipsel-linux-gnu /etc/qemu-binfmt/mipsel
 ln -s /usr/arm-linux-gnueabihf /etc/qemu-binfmt/arm
-rm /etc/apt/sources.list.d/emdebian.list
 
 # These are so the 64 bit vm can build 32 bit
 sudo apt-get -y install libx32gcc-4.8-dev
@@ -48,16 +45,16 @@ mkdir tools
 cd tools
 
 # Capstone for pwndbg
-git clone https://github.com/aquynh/capstone
-pushd capstone
-git checkout -t origin/next
-sudo ./make.sh install
-cd bindings/python
-sudo python3 setup.py install # Ubuntu 14.04+, GDB uses Python3
-popd
+#git clone https://github.com/aquynh/capstone
+#pushd capstone
+#git checkout -t origin/next
+#sudo ./make.sh install
+#cd bindings/python
+#sudo python3 setup.py install # Ubuntu 14.04+, GDB uses Python3
+#popd
 
 # pycparser for pwndbg
-sudo pip3 install pycparser # Use pip3 for Python3
+#sudo pip3 install pycparser # Use pip3 for Python3
 
 # Install radare2
 git clone https://github.com/radare/radare2
@@ -72,7 +69,7 @@ sudo python setup.py install
 popd
 
 # Uninstall capstone
-sudo pip2 uninstall capstone -y
+#sudo pip2 uninstall capstone -y
 
 # Install correct capstone
 pushd capstone/bindings/python
@@ -92,25 +89,25 @@ sudo apt-get -y install zsh
 echo vagrant | sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
 # tmux
-wget https://github.com/tmux/tmux/releases/download/2.5/tmux-2.5.tar.gz
-tar -zxvf tmux-2.5.tar.gz
-pushd tmux-2.5
-sudo apt-get -y install libevent-dev
-sudo apt-get -y install libncurses-dev
-./configure && make && sudo make install
-popd
-rm -rf tmux-2.5.tar.gz
-rm -rf tmux-2.5
+#wget https://github.com/tmux/tmux/releases/download/2.5/tmux-2.5.tar.gz
+#tar -zxvf tmux-2.5.tar.gz
+#pushd tmux-2.5
+#sudo apt-get -y install libevent-dev
+#sudo apt-get -y install libncurses-dev
+#./configure && make && sudo make install
+#popd
+#rm -rf tmux-2.5.tar.gz
+#rm -rf tmux-2.5
 
 # fzf
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
+#git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+#~/.fzf/install
 
 ## GDB Tools
 # Install peda
-git clone https://github.com/longld/peda.git
+#git clone https://github.com/longld/peda.git
 # Install pwndbg
-git clone https://github.com/zachriggle/pwndbg
+#git clone https://github.com/zachriggle/pwndbg
 # Install gef
 git clone https://github.com/hugsy/gef.git
 # Install voltron
@@ -120,9 +117,9 @@ pushd voltron
 popd
 
 # fixenv
-wget https://raw.githubusercontent.com/hellman/fixenv/master/r.sh
-mv r.sh fixenv
-chmod +x fixenv
+#wget https://raw.githubusercontent.com/hellman/fixenv/master/r.sh
+#mv r.sh fixenv
+#chmod +x fixenv
 
 # AFL Fuzzer
 wget http://lcamtuf.coredump.cx/afl/releases/afl-latest.tgz
